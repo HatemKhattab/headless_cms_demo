22cfeac104014059bcf9b947f4efedb612e1ad9d
Hatem Khattab <hattemkhattab@gmail.com>
2019-06-18 15:34:19 +0200

    add storyblok client

--- app/controllers/story_blok_controller.rb
+++ app/controllers/story_blok_controller.rb
@@ -1,4 +1,12 @@
 class StoryBlokController < ApplicationController
+  before_action :client
   def index
+  end
+
+  private
+
+  def client
+  #  @client ||= Storyblok::Client.new(token: ENV['STORY_BLOK_TOKEN'])
+    @client ||= Storyblok::Client.new(token: '5sd4d34s3d43')
   end
 end
