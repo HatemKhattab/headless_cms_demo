7d84330e08a6ccb1ab6d4cddc09b1c06800bca0c
Hatem Khattab <hattemkhattab@gmail.com>
2019-06-19 13:19:02 +0200

    view blog post

--- app/controllers/story_blok_controller.rb
+++ app/controllers/story_blok_controller.rb
@@ -6,7 +6,6 @@ class StoryBlokController < ApplicationController
   private
 
   def client
-  #  @client ||= Storyblok::Client.new(token: ENV['STORY_BLOK_TOKEN'])
-    @client ||= Storyblok::Client.new(token: '5sd4d34s3d43')
+     @client ||= Storyblok::Client.new(token: ENV['STORY_BLOK_TOKEN'], version: 'published')
   end
 end
